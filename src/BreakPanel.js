bbb3f4d9b670f0ab5702fd3084a8691d1aed6a5b
LeviaThanSr <omarsarour@gmail.com>
2021-02-18 17:59:55 +0100

    Create BreakPanel

---
+++ src/BreakPanel.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class BreakPanel extends Component {
+  render() {
+    return (
+      <div className="controlPanel text-center">
+        <h1 className="name text-center">{this.props.name}</h1>
+        <div className="controlBtn  d-flex justify-content-center align-items-center">
+          <div className="btn btn-danger" onClick={this.props.handleDecrement}>
+            <i className="fas fa-minus" />
+          </div>
+          <h3 className="time">{this.props.time}</h3>
+          <div className="btn btn-success" onClick={this.props.handleIncrement}>
+            <i className="fas fa-plus" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BreakPanel;
