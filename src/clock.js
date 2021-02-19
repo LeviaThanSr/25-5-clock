eac821b8eafbb9fa3aa5caed98968ad0448c5d2d
LeviaThanSr <omarsarour@gmail.com>
2021-02-19 05:06:24 +0100

    Update 3

--- src/clock.js
+++ src/clock.js
@@ -2,20 +2,103 @@ import React, { Component } from "react";
 import BreakPanel from "./BreakPanel";
 import SessionPanel from "./SessionPanel";
 import ClockPanel from "./ClockPanel";
+
+const beep = document.getElementById("beep");
 class Clock extends Component {
   constructor(props) {
     super(props);
+    this.counter = undefined;
     this.state = {
       BreakLength: 5,
       SessionLength: 25,
-      ClockTime: 25 * 60,
-      ClockTitle: "Session Title",
+      ClockTime: 1500,
+      ClockTitle: "Session",
+      isOn: false,
     };
+    this.handleReset = this.handleReset.bind(this);
+    this.handleResumePause = this.handleResumePause.bind(this);
+    this.handleBreakIncrement = this.handleBreakIncrement.bind(this);
+    this.handleBreakDecrement = this.handleBreakDecrement.bind(this);
+    this.handleSessionIncrement = this.handleSessionIncrement.bind(this);
+    this.handleSessionDecrement = this.handleSessionDecrement.bind(this);
   }
+
   handleConvert(time) {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
     return `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+  }
+  handleReset() {
+    this.setState({
+      BreakLength: 5,
+      SessionLength: 25,
+      ClockTime: 1500,
+      ClockTitle: "Session",
+      isOn: false,
+    });
+    clearInterval(this.counter);
+    beep.pause();
+    beep.currentTime = 0;
+  }
+  componentWillUnmount() {
+    clearInterval(this.counter);
+  }
+  handleResumePause() {
+    if (!this.state.isOn) {
+      this.counter = setInterval(() => {
+        if (this.state.ClockTime === 0) {
+          this.setState({
+            ClockTitle:
+              this.state.ClockTitle === "Session" ? "Break" : "Session",
+            ClockTime:
+              this.state.ClockTitle === "Session"
+                ? this.state.BreakLength * 60
+                : this.state.SessionLength * 60,
+          });
+          beep.play();
+        } else {
+          this.setState({
+            ClockTime: this.state.ClockTime - 1,
+          });
+        }
+      }, 1000);
+      this.setState({
+        isOn: true,
+      });
+    } else {
+      clearInterval(this.counter);
+      this.setState({
+        isOn: false,
+      });
+    }
+  }
+  handleBreakIncrement() {
+    if (this.state.BreakLength <= 59) {
+      this.setState({
+        BreakLength: this.state.BreakLength + 1,
+      });
+    }
+  }
+  handleBreakDecrement() {
+    if (this.state.BreakLength > 1) {
+      this.setState({
+        BreakLength: this.state.BreakLength - 1,
+      });
+    }
+  }
+  handleSessionIncrement() {
+    if (this.state.SessionLength <= 59) {
+      this.setState({
+        SessionLength: this.state.SessionLength + 1,
+      });
+    }
+  }
+  handleSessionDecrement() {
+    if (this.state.SessionLength > 1) {
+      this.setState({
+        SessionLength: this.state.SessionLength - 1,
+      });
+    }
   }
   render() {
     const Break = {
@@ -30,32 +113,24 @@ class Clock extends Component {
       handleIncrement: this.handleSessionIncrement,
       handleDecrement: this.handleSessionDecrement,
     };
+    const Clock = {
+      ClockTitle: this.state.ClockTitle,
+      ClockTime: this.handleConvert(this.state.ClockTime),
+      handleResumePause: this.handleResumePause,
+      handleReset: this.handleReset,
+      isOn: this.state.isOn,
+    };
     return (
       <div>
         <div className="card shadow rounded">
           <div className="card-header">
             <div className="d-flex justify-content-center align-items-center">
-              <BreakPanel
-                name={Break.name}
-                time={Break.time}
-                handleIncrement={Break.handleIncrement}
-                handleDecrement={Break.handleDecrement}
-              />
-              <SessionPanel
-                name={Session.name}
-                time={Session.time}
-                handleIncrement={Session.handleIncrement}
-                handleDecrement={Session.handleDecrement}
-              />
+              <BreakPanel {...Break} />
+              <SessionPanel {...Session} />
             </div>
           </div>
           <div className="card-body">
-            <ClockPanel
-              ClockTitle={this.state.ClockTitle}
-              ClockTime={this.handleConvert(this.state.ClockTime)}
-              handleResumePause={this.handleResumePause}
-              handleReset={this.handleReset}
-            />
+            <ClockPanel {...Clock} />
           </div>
         </div>
       </div>
