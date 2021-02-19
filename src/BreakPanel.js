e48ca187e53c84c74384c0843a220448acefd384
LeviaThanSr <omarsarour@gmail.com>
2021-02-19 06:09:20 +0100

    Update BreakPanel

--- src/BreakPanel.js
+++ src/BreakPanel.js
@@ -7,12 +7,22 @@ class BreakPanel extends Component {
         <h1 className="name text-center" id="break-label">
           {this.props.name}
         </h1>
-        <div className="controlBtn  d-flex justify-content-center align-items-center">
-          <div className="btn btn-danger" onClick={this.props.handleDecrement}>
+        <div className="controlBtn d-flex justify-content-center align-items-center">
+          <div
+            className="btn btn-danger"
+            id="break-decrement"
+            onClick={this.props.handleDecrement}
+          >
             <i className="fas fa-minus" />
           </div>
-          <h3 className="time">{this.props.time}</h3>
-          <div className="btn btn-primary" onClick={this.props.handleIncrement}>
+          <h3 className="time" id="break-length">
+            {this.props.time}
+          </h3>
+          <div
+            className="btn btn-primary"
+            id="break-increment"
+            onClick={this.props.handleIncrement}
+          >
             <i className="fas fa-plus" />
           </div>
         </div>
