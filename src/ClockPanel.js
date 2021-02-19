c5ff761a512a205bbb8bf1275fb73d3f98d2c490
LeviaThanSr <omarsarour@gmail.com>
2021-02-19 08:12:14 +0100

    Update ClockPanel

--- src/ClockPanel.js
+++ src/ClockPanel.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react";
-
 class ClockPanel extends Component {
   render() {
+    const {
+      isItLessThanOne,
+      ClockTime,
+      ClockTitle,
+      isOn,
+      handleResumePause,
+      handleReset,
+    } = this.props;
+
     return (
       <div className="clockPanel text-center">
-        <h1 className="title" id="timer-label">
-          {this.props.ClockTitle}
+        <h1
+          className="title"
+          id="timer-label"
+          style={{ color: isItLessThanOne < 60 ? "red" : "black" }}
+        >
+          {ClockTitle}
         </h1>
-        <h3 className="time" id="time-left">
-          {this.props.ClockTime}
+        <h3
+          className="time"
+          id="time-left"
+          style={{ color: isItLessThanOne < 60 ? "red" : "black" }}
+        >
+          {ClockTime}
         </h3>
         <div className="controlBtn2 d-flex justify-content-center align-items-center">
           <div
             className="btn btn-primary"
             id="start_stop"
-            onClick={this.props.handleResumePause}
+            onClick={handleResumePause}
           >
-            <i className={`fas fa-${this.props.isOn ? "pause" : "play"}`} />
+            <i className={`fas fa-${isOn ? "pause" : "play"}`} />
           </div>
 
-          <div
-            className="btn btn-warning"
-            id="reset"
-            onClick={this.props.handleReset}
-          >
+          <div className="btn btn-warning" id="reset" onClick={handleReset}>
             <i className="fas fa-undo" />
           </div>
         </div>
