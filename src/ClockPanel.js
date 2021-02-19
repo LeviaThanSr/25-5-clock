f1922a98abed3cf6da507e7ae0ee6f13738daf35
LeviaThanSr <omarsarour@gmail.com>
2021-02-19 06:09:31 +0100

    Update ClockPanel

--- src/ClockPanel.js
+++ src/ClockPanel.js
@@ -4,17 +4,26 @@ class ClockPanel extends Component {
   render() {
     return (
       <div className="clockPanel text-center">
-        <h1 className="title">{this.props.ClockTitle}</h1>
-        <h3 className="time">{this.props.ClockTime}</h3>
+        <h1 className="title" id="timer-label">
+          {this.props.ClockTitle}
+        </h1>
+        <h3 className="time" id="time-left">
+          {this.props.ClockTime}
+        </h3>
         <div className="controlBtn2 d-flex justify-content-center align-items-center">
           <div
             className="btn btn-primary"
+            id="start_stop"
             onClick={this.props.handleResumePause}
           >
             <i className={`fas fa-${this.props.isOn ? "pause" : "play"}`} />
           </div>
 
-          <div className="btn btn-warning" onClick={this.props.handleReset}>
+          <div
+            className="btn btn-warning"
+            id="reset"
+            onClick={this.props.handleReset}
+          >
             <i className="fas fa-undo" />
           </div>
         </div>
