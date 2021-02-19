741c543faa3427c1676c0a9a46c3e9d522430bc9
LeviaThanSr <omarsarour@gmail.com>
2021-02-19 05:05:41 +0100

    Update SessionPanel

--- src/SessionPanel.js
+++ src/SessionPanel.js
@@ -4,7 +4,9 @@ class SessionPanel extends Component {
   render() {
     return (
       <div className="controlPanel text-center">
-        <h1 className="name text-center">{this.props.name}</h1>
+        <h1 className="name text-center" id="session-label">
+          {this.props.name}
+        </h1>
         <div className="controlBtn  d-flex justify-content-center align-items-center">
           <div className="btn btn-danger" onClick={this.props.handleDecrement}>
             <i className="fas fa-minus" />
