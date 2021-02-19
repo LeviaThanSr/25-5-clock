200c4a2a9c42b0dc1293d6a46a2b967b50fb32b6
LeviaThanSr <omarsarour@gmail.com>
2021-02-19 08:12:02 +0100

    Update SessionPanel

--- src/SessionPanel.js
+++ src/SessionPanel.js
@@ -2,26 +2,28 @@ import React, { Component } from "react";
 
 class SessionPanel extends Component {
   render() {
+    const { name, time, handleIncrement, handleDecrement } = this.props;
+
     return (
       <div className="controlPanel text-center">
         <h1 className="name text-center" id="session-label">
-          {this.props.name}
+          {name}
         </h1>
         <div className="controlBtn  d-flex justify-content-center align-items-center">
           <div
             className="btn btn-danger"
             id="session-decrement"
-            onClick={this.props.handleDecrement}
+            onClick={handleDecrement}
           >
             <i className="fas fa-minus" />
           </div>
           <h3 className="time" id="session-length">
-            {this.props.time}
+            {time}
           </h3>
           <div
             className="btn btn-primary"
             id="session-increment"
-            onClick={this.props.handleIncrement}
+            onClick={handleIncrement}
           >
             <i className="fas fa-plus" />
           </div>
